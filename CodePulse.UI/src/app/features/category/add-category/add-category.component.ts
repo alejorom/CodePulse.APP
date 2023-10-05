82599afc453866a75bcf17fe7c5530ca5732324b
rromerro900814094 <rromeror@indracompany.com>
2023-10-05 17:51:18 -0500

    Crear unsubscribe from observables

--- CodePulse.UI/src/app/features/category/add-category/add-category.component.ts
+++ CodePulse.UI/src/app/features/category/add-category/add-category.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscription? : Subscription
 
   constructor(private categoryService: CategoryService) {
     this.model = {
@@ -19,11 +21,15 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit() {
-    this.categoryService.addCategory(this.model)
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
       .subscribe({
         next: (response) => {
           console.log('Esto fue exitoso!');
         }
       });
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
