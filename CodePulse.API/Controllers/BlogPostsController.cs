308a8c68b21595f37bbe831cd1145da1909c87c0
rromerro900814094 <rromeror@indracompany.com>
2023-11-08 11:57:43 -0500

    Actualizar Blogpost para recuperar categories

--- CodePulse.API/Controllers/BlogPostsController.cs
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -94,7 +94,13 @@ namespace CodePulse.API.Controllers
                     PublishedDate = blogPost.PublishedDate,
                     ShortDescription = blogPost.ShortDescription,
                     Title = blogPost.Title,
-                    UrlHandle = blogPost.UrlHandle
+                    UrlHandle = blogPost.UrlHandle,
+                    Categories = blogPost.Categories.Select(x => new CategoryDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        UrlHandle = x.UrlHandle
+                    }).ToList()
                 });
             }
 
