8c9f886160c26bec20139121c1f1d0f5cbe2bfc5
rromerro900814094 <rromeror@indracompany.com>
2023-11-08 15:00:28 -0500

    Submit Categories Add-Blogpost

--- CodePulse.UI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ CodePulse.UI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -27,7 +27,8 @@ export class AddBlogpostComponent implements OnInit{
       featuredImageUrl: '',
       author: '',
       isVisible: true,
-      publishedDate: new Date()
+      publishedDate: new Date(),
+      categories: []
     }
   }
   ngOnInit(): void {
